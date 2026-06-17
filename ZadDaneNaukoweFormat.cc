6a2f64fcb949d1ba1a8ad77a6d3d601d1bbb1c4d
Drama226 <polak226pl@gmail.com>
2026-06-17 13:04:47 +0200

    rozwiązanie konfliktu

--- ZadDaneNaukoweFormat.cc
+++ ZadDaneNaukoweFormat.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -7,13 +6,14 @@ using namespace std;
 
 int main()
 {
-    const double pi   = M_PI;      
-    const double e    = M_E;         
-    const double phi  = (1 + sqrt(5)) / 2; 
+    const double pi  = M_PI;
+    const double e   = M_E;
+    const double phi = (1 + sqrt(5)) / 2;
 
     cout << fixed << setprecision(6);
 
     int space[4] = {15,25,20,25};
+
     cout << setw(15) << left << "Stala"
          << setw(25) << "Wartosc domyslna"
          << setw(20) << "Notacja stala"
@@ -32,7 +32,7 @@ int main()
     cout << setw(space[0]) << left << "e"
          << setw(space[1]) << M_E
          << setw(space[2]) << M_E
-         << scientific << setw(space[4]) << M_E
+         << scientific << setw(space[3]) << M_E
          << endl;
 
     cout << fixed;
@@ -40,55 +40,8 @@ int main()
     cout << setw(space[0]) << left << "Zloty podzial"
          << setw(space[1]) << phi
          << setw(space[2]) << phi
-         << scientific << setw(space[4]) << phi
+         << scientific << setw(space[3]) << phi
          << endl;
 
     return 0;
-=======
-#include <iostream>
-#include <iomanip>
-#include <cmath>
-
-using namespace std;
-
-int main()
-{
-    const double pi   = M_PI;      
-    const double e    = M_E;         
-    const double phi  = (1 + sqrt(5)) / 2; 
-
-    cout << fixed << setprecision(6);
-
-    int space[4] = {15,25,20,25};
-    cout << setw(15) << left << "Stala"
-         << setw(25) << "Wartosc domyslna"
-         << setw(20) << "Notacja stala"
-         << setw(25) << "Notacja naukowa" << endl;
-
-    cout << string(85, '-') << endl;
-
-    cout << setw(space[0]) << left << "Pi"
-         << setw(space[1]) << M_PI
-         << setw(space[2]) << M_PI
-         << scientific << setw(space[3]) << M_PI
-         << endl;
-
-    cout << fixed;
-
-    cout << setw(space[0]) << left << "e"
-         << setw(space[1]) << M_E
-         << setw(space[2]) << M_E
-         << scientific << setw(space[4]) << M_E
-         << endl;
-
-    cout << fixed;
-
-    cout << setw(space[0]) << left << "Zloty podzial"
-         << setw(space[1]) << phi
-         << setw(space[2]) << phi
-         << scientific << setw(space[4]) << phi
-         << endl;
-
-    return 0;
->>>>>>> b9a64a71f9f8d086e0227ce74564a32a4c14bb71
 }
