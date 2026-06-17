83ef0cc122486f6016c9dad8c1bcad3120c3cda1
Drama226 <polak226pl@gmail.com>
2026-06-17 13:06:10 +0200

    rozwiązanie konfliktu

--- LKształt.cc
+++ LKształt.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -58,65 +57,4 @@ int main()
     }
 
     return 0;
-=======
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int rozmiar;
-
-    cout << "Podaj rozmiar: ";
-    cin >> rozmiar;
-
-    cout << "\nKwadrat:" << endl;
-    for (int i = 0; i < rozmiar; i++)
-    {
-        for (int j = 0; j < rozmiar; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    cout << "\nTrojkat prostokatny:" << endl;
-    for (int i = 0; i < rozmiar; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    cout << "\nTrojkat rownoramienny:" << endl;
-    for (int i = 0; i < rozmiar; i++)
-    {
-        for (int j = 0; j < rozmiar - i - 1; j++)
-        {
-            cout << " ";
-        }
-
-        for (int j = 0; j < 2 * i + 1; j++)
-        {
-            cout << "*";
-        }
-
-        cout << endl;
-    }
-
-    cout << "\nPiramida z liczb:" << endl;
-    for (int i = 1; i <= rozmiar; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << i << " ";
-        }
-
-        cout << endl;
-    }
-
-    return 0;
->>>>>>> b9a64a71f9f8d086e0227ce74564a32a4c14bb71
 }
--- ZadFriend.cc
+++ ZadFriend.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -53,60 +52,4 @@ int main() {
     cout << "A NIE ma dostepu do B (brak friend w druga strone)" << endl;
 
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-class B;
-
-class A {
-private:
-    int tajneA;
-
-public:
-    A() : tajneA(10) {}
-
-    friend class B;
-};
-
-class B {
-private:
-    int tajneB;
-
-public:
-    B() : tajneB(20) {}
-
-    void pokazA(A& a) {
-        cout << "B widzi tajneA = " << a.tajneA << endl;
-    }
-
-    void pokazB() {
-        cout << "tajneB = " << tajneB << endl;
-    }
-
-    void probujDostepDoA(A& a) {
-        cout << "Proba dostepu do A z B: " << a.tajneA << endl;
-    }
-};
-
-int main() {
-
-    A a;
-    B b;
-
-    cout << "=== Dostep B do A (friend) ===" << endl;
-    b.pokazA(a);
-
-    cout << endl;
-
-    cout << "=== Dostep B do swoich danych ===" << endl;
-    b.pokazB();
-
-    cout << endl;
-
-    cout << "=== Wniosek ===" << endl;
-    cout << "A NIE ma dostepu do B (brak friend w druga strone)" << endl;
-
-    return 0;
->>>>>>> b9a64a71f9f8d086e0227ce74564a32a4c14bb71
 }
